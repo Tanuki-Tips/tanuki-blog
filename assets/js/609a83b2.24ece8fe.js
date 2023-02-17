d0bf0837afaee9ecf6b9cc33774d5aedd8617695
AntoineSmet <AntoineSmet@users.noreply.github.com>
2023-02-17 11:10:36 +0000

    deploy: b160cd029f4827c75f4e61196004a837bdb7aee9

---
+++ assets/js/609a83b2.24ece8fe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktanuki_blog=self.webpackChunktanuki_blog||[]).push([[712],{8957:e=>{e.exports=JSON.parse('{"label":"Cogntive-Service","permalink":"/blog/tags/cogntive-service","allTagsPath":"/blog/tags","count":1}')}}]);
